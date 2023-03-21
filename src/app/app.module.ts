1c86c8aba7dc9f2979fb3d3c06055eb54f28b63d
Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-03-21 12:47:56 +0600

    Migrated from css to scss
    Also removed boiler plate codes from app html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {DoctorModule} from "./doctor/doctor.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DoctorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
