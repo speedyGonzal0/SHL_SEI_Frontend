20f41eda71fa2d2d3994d7c6271d594e3b6b387e
Samin Ahsan <54761093+speedyGonzal0@users.noreply.github.com>
2023-03-21 12:14:13 +0600

    created login/logout component, auth module, initialized auth guard/service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import {AuthenticationModule} from "./authentication/authentication.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AuthenticationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
