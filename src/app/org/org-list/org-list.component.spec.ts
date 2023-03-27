eba5348ec6e5ea7f6e83714a17893461e1c91c5d
Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2023-03-27 13:52:37 +0600

    Org list boilerplate

---
+++ src/app/org/org-list/org-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrgListComponent } from './org-list.component';
+
+describe('OrgListComponent', () => {
+  let component: OrgListComponent;
+  let fixture: ComponentFixture<OrgListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OrgListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OrgListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
