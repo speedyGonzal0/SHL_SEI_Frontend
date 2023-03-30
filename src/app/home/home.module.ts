199a996b745be0ddb4640dfffec0f7285f636956
Samin Ahsan <54761093+speedyGonzal0@users.noreply.github.com>
2023-03-30 10:06:35 +0600

    homepage layout overhaul

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+
+
 import { CommonModule } from '@angular/common';
-
-
 import { HomeComponent } from '@home/home/home.component';
 import { TopbarComponent } from '@home/topbar/topbar.component';
 import { SidebarComponent } from '@home/sidebar/sidebar.component';
-import {AuthenticationModule} from "@authentication/authentication.module";
-
-
-import {ButtonModule} from 'primeng/button';
-import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
-import {DoctorModule} from "@doctor/doctor.module";
-import {DiagnosticsModule} from "@diagnostics/diagnostics.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
+import {AuthenticationModule} from "@authentication/authentication.module";
+import {DoctorModule} from "@doctor/doctor.module";
+
+import {DiagnosticsModule} from "@diagnostics/diagnostics.module";
+import {ButtonModule} from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
 
 
 
@@ -32,6 +32,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
         CommonModule,
         AuthenticationModule,
         ButtonModule,
+        SidebarModule,
         RouterLink,
         RouterOutlet,
         RouterLinkActive
