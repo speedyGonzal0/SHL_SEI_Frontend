8cb8920580f3edc1be4a57a11a587b0bbccc3aea
Samin Ahsan <54761093+speedyGonzal0@users.noreply.github.com>
2023-03-30 10:59:46 +0600

    changed logout component  appearance

--- src/app/authentication/logout/logout.component.ts
+++ src/app/authentication/logout/logout.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import {ConfirmationService} from 'primeng/api';
+import {Component, OnInit} from '@angular/core';
+import {ConfirmationService, MenuItem} from 'primeng/api';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss']
 })
-export class LogoutComponent {
+export class LogoutComponent implements OnInit{
+
+  menuItems !: MenuItem[];
 
   constructor(private confirmationService: ConfirmationService){
+  }
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Signed in as ADMIN',
+        escape: false,
+        disabled: true
+      },
+      {
+        label: 'Admin name',
+        escape: false,
+        disabled: true
+      },
+      {
+        label: 'Log out',
+        icon: "pi pi-sign-out",
+        command: () => this.confirm()
+      }
+    ];
   }
 
   confirm() {
